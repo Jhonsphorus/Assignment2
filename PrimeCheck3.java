3e40fee2c8ec323eea0b824a7d659fe0a5b15532
unknown <b.adejohnson@hotmail.com>
2019-05-22 11:18:39 +0100

    Code for checking prime number

---
+++ PrimeCheck3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeCheck{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number to check: ");
+        int num1 = input.nextInt();
+
+        for (int i =2; i<=num1; i++){
+
+            if (num1%i ==0 && i != num1 ){
+                System.out.println(num1 + " is not a Prime number");
+                break;
+            }
+            //else if (num1 % i !=0 ){
+
+            //}
+            else {
+                System.out.println(num1 + " is a Prime number");
+                break;
+            }
+
+        }
+    }
+}
